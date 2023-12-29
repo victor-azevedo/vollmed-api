6a90dc34653a052e3677cd38a487713acb658989
Victor Azevedo <josevcazevedo@gmail.com>
2023-12-29 15:08:42 -0300

    style: project format

--- src/main/java/br/com/victor/vollmed/api/VollmedApplication.java
+++ src/main/java/br/com/victor/vollmed/api/VollmedApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VollmedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VollmedApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(VollmedApplication.class, args);
+  }
 }
--- src/test/java/br/com/victor/vollmed/api/VollmedApplicationTests.java
+++ src/test/java/br/com/victor/vollmed/api/VollmedApplicationTests.java
@@ -5,9 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VollmedApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
