292cb2d278ba8bd7b20c6706ac8de01707516510
Victor Azevedo <josevcazevedo@gmail.com>
2023-12-29 14:51:34 -0300

    initial commit

---
+++ src/test/java/br/com/victor/vollmed/api/VollmedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor.vollmed.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VollmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
