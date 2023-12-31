988d8ed63697a3d60055c00b06c16c87eb46a543
Victor Azevedo <josevcazevedo@gmail.com>
2023-12-31 15:23:47 -0300

    feat: add GET /doctors

--- src/main/java/br/com/victor/vollmed/api/controllers/DoctorController.java
+++ src/main/java/br/com/victor/vollmed/api/controllers/DoctorController.java
@@ -9,10 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @Validated
@@ -25,5 +23,11 @@ public class DoctorController {
   public ResponseEntity<HttpStatus> register(@Valid @RequestBody DoctorDTO doctorDTO) {
     doctorRepository.save(new DoctorModel(doctorDTO));
     return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @GetMapping
+  public ResponseEntity<List<DoctorModel>> getAllDoctors() {
+    List<DoctorModel> doctorList = doctorRepository.findAll();
+    return ResponseEntity.status(HttpStatus.OK).body(doctorList);
   }
 }
