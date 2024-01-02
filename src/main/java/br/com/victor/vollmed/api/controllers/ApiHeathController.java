8fae33f2df0f7d5ae6e73ec257bd0fd567968a15
Victor Azevedo <josevcazevedo@gmail.com>
2024-01-02 09:44:32 -0300

    refactor: response GET /health

--- src/main/java/br/com/victor/vollmed/api/controllers/ApiHeathController.java
+++ src/main/java/br/com/victor/vollmed/api/controllers/ApiHeathController.java
@@ -1,5 +1,6 @@
 package br.com.victor.vollmed.api.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class ApiHeathController {
   @GetMapping
-  public String health() {
-    return "VollMed API healthy";
+  public ResponseEntity<String> health() {
+    return ResponseEntity.ok("VollMed API healthy");
   }
 }
